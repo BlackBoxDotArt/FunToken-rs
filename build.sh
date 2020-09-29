6d6d195398aa82526f7b8aa806b015c9bc473e48
SRead <39156178+SLRead@users.noreply.github.com>
2020-09-29 18:55:03 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -e
 cd "`dirname $0`"
 source ../flags.sh
 cargo build --target wasm32-unknown-unknown --release
-cp target/wasm32-unknown-unknown/release/lockable_fungible_token.wasm ./res/
+cp target/wasm32-unknown-unknown/release/mintable_fungible_token.wasm ./res/
