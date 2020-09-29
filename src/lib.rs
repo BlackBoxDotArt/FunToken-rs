47775ab27adf37f9fb195844d86b69f482088bdb
SRead <39156178+SLRead@users.noreply.github.com>
2020-09-29 07:53:15 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,14 @@
 * Mintable Fungible Token implementation with JSON serialization. 
 *
 * Based on Mintable Fungible Token for Rainbow Bridge: https://github.com/near/rainbow-bridge-rs/tree/master/mintable-fungible-token
-* This contract will implement $MENTA the token of Hackumenta - A full Art Fair organized peer to peer and funded through a token sale.
-* Every interaction with $MENTA (mint, burn, transfer) will microfund the Hackumenta Pool for the Art Fair.
+* This contract will implement $NMT (Near $MENTA) the token of Hackumenta - A full Art Fair organized peer to peer and funded through a token sale.
+* Every interaction with $MENTA (mint, burn, transfer) will microfund the Hackumenta Pool for the Art Fair. 
+* Proceeds are completely dedicated to fund the Fair effectively acting as an “Art Pre Sale” - or more cheekly an Initial Art Offering.
+*
+* The goal of this token is to mirror the economics of TrojanToken.sol and mirror $MENTA across the Rainbow Bridge so that participants can
+* take advantage of the lower costs and faster speeds of the Near blockchain. 
+*
+* This Fungible Token is the foundation stone upon which real life art experiences are built.
 *
 * NOTES:
 Properties specific to Mintable Fungible Token:
